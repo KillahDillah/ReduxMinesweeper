e81bcdd3f8fa460d037616d94129a652eb6b8139
Leah Miller <leahmiller1969@gmail.com>
2021-07-30 21:58:27 -0700

    board displaying

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,24 +1,22 @@
+import React from "react";
 import { useSelector } from "react-redux";
 
 export const Gameboard = () => {
   const width = 16;
+  const squares = [];
   const mines = useSelector((state) => state.mines);
   const minesArray = Array(mines).fill("mine");
   const emptyArray = Array(width * width - mines).fill("empty");
   const gameArray = emptyArray.concat(minesArray);
   const boardArray = gameArray.sort(() => Math.random() - 0.5);
 
-  console.log(boardArray);
-
   function createBoard() {
-    for (let x = 0; x < width * width; x++) {
-      let row = [];
-      for (let y = 0; y < width * width; y++) {
-        let square = [];
-      }
+    for (let i = 0; i < width * width; i++) {
+      // use styled component
+      squares.push(<div className={boardArray[i]}></div>);
     }
   }
   createBoard();
 
-  return <div className="board">{mines}</div>;
+  return <div className="board">{squares}</div>;
 };
